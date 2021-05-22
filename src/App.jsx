5d4ad727ad398d581b674147199b4b37b1bbe630
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-05-22 20:10:39 +0500

    Add Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import useSocket from './Hooks/useSocket';
 import './App.css';
 
 function App() {
 
   const [ message, setMessage ] = useState('')
+  const [ messages, setMessages ] = useState([])
 
   const socket = useSocket()
+
+  useEffect(() => {
+
+    socket.on('reseive_message', message => {
+      setMessages((value) => [message, ...value])
+    })
+
+  }, [socket])
 
   return (
     <>
       <input type="text" onKeyUp={(e) => setMessage(e.target.value)} />
 
       <button onClick={() => socket.emit('send_mesage', message)}>Send</button>
+
+      {
+        messages.map((m, i) => (
+          <li key={i}>{m}</li>
+        ))
+      }
     </>
   )
 }
